b019ce1779e59e1694f23518e9ea0dba911dfff2
Enrique <git.eegee@0wnd.org>
2022-04-29 22:09:24 +0200

    Bump Storybook to v6.4.22
    > npx sb upgrade
     • Checking for latest versions of '@storybook/*' packagesinfo ,Upgrading C:\dev\Assupol\angular-material-storybook\package.json
    info
    info All dependencies match the latest package versions :)
    info ,
    info
     • Installing upgrades • Preparing to install dependencies. ✓
    
    removed 11 packages, and audited 2259 packages in 30s
    
    257 packages are looking for funding
      run `npm fund` for details
    
    31 vulnerabilities (1 low, 6 moderate, 24 high)
    
    To address issues that do not require attention, run:
      npm audit fix
    
    To address all issues (including breaking changes), run:
      npm audit fix --force
    
    Run `npm audit` for details.
    . ✓
    WARN Found 2 outdated packages (relative to '@storybook/addon-actions@6.4.22')
    WARN Please make sure your packages are updated to ensure a consistent experience.
    WARN - @storybook/addon-notes@5.3.21
    WARN - @storybook/addon-knobs@5.3.19
    WARN Found 1 deprecated packages since 6.0.0-alpha.0
    WARN See https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#60-deprecations
    WARN - @storybook/addon-notes@5.3.21
    🔎 checking 'cra5'
    🔎 checking 'webpack5'
    🔎 checking 'angular12'
    ╭─────────────────────────────────────────────────────────────────────────────────────────────────────╮
    │                                                                                                     │
    │   We've detected you are running Angular ~13.3.5 which is powered by webpack5.                      │
    │   Storybook ^6.4.22 runs webpack4 by default, which is incompatible.                                │
    │                                                                                                     │
    │   In order to work with your version of Angular, we need to install Storybook's webpack5 builder.   │
    │                                                                                                     │
    │   More info: https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#angular12-upgrade      │
    │                                                                                                     │
    ╰─────────────────────────────────────────────────────────────────────────────────────────────────────╯
    √ Do you want to run the 'angular12' fix on your project? ... yes
    ✅ Adding dependencies: @storybook/manager-webpack5@^6.4.22,@storybook/builder-webpack5@^6.4.22,webpack@5
    
    added 349 packages, removed 55 packages, changed 40 packages, and audited 2553 packages in 42s
    
    265 packages are looking for funding
      run `npm fund` for details
    
    29 vulnerabilities (1 low, 5 moderate, 23 high)
    
    To address issues that do not require attention, run:
      npm audit fix
    
    To address all issues (including breaking changes), run:
      npm audit fix --force
    
    Run `npm audit` for details.
    ✅ Setting `core.builder` to `webpack5` in main.js
    ✅ fixed angular12
    🔎 checking 'mainjsFramework'
    🔎 checking 'eslintPlugin'
    🔎 checking 'builder-vite'
    Not using community vite builder, skipping

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.ts'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes', '@storybook/addon-knobs/register'],
+  core: {
+    builder: 'webpack5'
+  }
 };
